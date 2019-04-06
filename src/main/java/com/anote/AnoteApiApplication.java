0560b57d79b0d251b54578e1b32389eee05406c8
Vitor Souza <dev_vitorsouza@hotmail.com>
2019-04-06 12:46:06 -0300

    first commit

---
+++ src/main/java/com/anote/AnoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.anote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnoteApiApplication.class, args);
+	}
+
+}
