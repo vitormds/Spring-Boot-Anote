7d8b52c0bbe53b783340fc20e469a42db4641ba7
Vitor Souza <dev_vitorsouza@hotmail.com>
2019-04-06 16:22:40 -0300

    Setting properties and settings

--- src/main/java/com/anote/model/Note.java
+++ src/main/java/com/anote/model/Note.java
@@ -1,13 +1,7 @@
 package com.anote.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
